7f3484d996d545dd7b11720aa6be3474fbec4dd3
rahulgajwani <rahulr.gajwani@cognizant.com>
2022-03-27 08:21:55 +0000

    done

--- tutorial-project-main/src/App.js
+++ tutorial-project-main/src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
-import LoginPage from "./components/LoginPage/LoginPage";
-import RegisterPage from "./components/RegisterPage/RegisterPage";
-import { Route } from "react-router-dom"
-
 
 function App() {
-  return (
-    <div>
-      
-        bye
-    </div>
-  );
+  return <div>hii</div>;
 }
 
 export default App;
